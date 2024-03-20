7bd3d0ba59cd33705a6337d164c39b1938f0bcc2
alxrgdriguez <sralexred@gmail.com>
2024-03-20 17:59:38 +0100

    ejemploPredicate

--- Tema7Prog/interfacesFuncionales/Predicate/ejemploPredicate.java
+++ Tema7Prog/interfacesFuncionales/Predicate/ejemploPredicate.java
@@ -3,6 +3,7 @@ package Tema7Prog.interfacesFuncionales.Predicate;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class ejemploPredicate {
 
@@ -13,11 +14,21 @@ public class ejemploPredicate {
 
         listaPersonas.add(new Persona("John","john@gmail.com", "646558887", 21));
         listaPersonas.add(new Persona("Alex","Alex@gmail.com", "546577727", 32));
-        listaPersonas.add(new Persona("Pepito","pepito@gmail.com", "946559087",20));
+        listaPersonas.add(new Persona("Pepita","pepito@gmail.com", "946559087",20));
         listaPersonas.add(new Persona("Manolito","manolito@gmail.com", "236558887",5));
 
         listaPersonas.stream()
                 .filter((p) -> p.getEdad() >= 21)
+                .forEach(System.out::println);
+
+        Predicate<Persona> edad = (p) -> p.getEdad() >= 21;
+        Predicate<Persona> nombre = (p) -> p.getNombre().endsWith("o");
+        Predicate<Persona> complejo = edad.or(nombre);
+
+        System.out.println("");
+        listaPersonas
+                .stream()
+                .filter(complejo)
                 .forEach(System.out::println);
 
 
