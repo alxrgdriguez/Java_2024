7d2b7ab01e6b0c76a320c3f2950a39b389dc39ef
alxrgdriguez <sralexred@gmail.com>
2024-04-22 20:25:18 +0200

    bd_spring_ejemplo

---
+++ Tema8Prog/database/appbd/src/main/java/com/jaroso/appbd/AppbdApplication.java
@@ -0,0 +1,13 @@
+package com.jaroso.appbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppbdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppbdApplication.class, args);
+    }
+
+}
---
+++ Tema8Prog/database/appbd/src/test/java/com/jaroso/appbd/AppbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaroso.appbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppbdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
